3bc18b759a5be77508ada9d62a75147da02bcb17
Bloody@DESKTOP-INU2790 <mathieu_franon@outlook.fr>
2018-06-18 11:13:47 +0200

    mise en place de la vue de connexion

---
+++ views/backend/connectionView.php
@@ -0,0 +1,26 @@
+<?php ob_start(); ?>
+<?php $title = 'Connexion'; ?>
+
+<div class="alert alert-success" role="alert">
+  	<?= $_SESSION['success']; ?>
+</div>
+
+<form action="index.php?action=connection" method="post">
+	<div class="form-row justify-content-center">
+		<div class="form-group col-8">
+			<label for="Title">Pseudo</label>
+			<input type="text" class="form-control" id="pseudo" name="pseudo">
+		</div>
+		<div class="form-group col-8">
+			<label for="Title">Mot de passe</label>
+			<input type="text" class="form-control" id="password" name="password">
+		</div>
+	</div>
+	<div id="connexion" class="text-center">
+		<button type="submit" class="btn btn-primary mb-2">Connexion</button>
+	</div>
+</form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('views\frontend\templateAdmin.php'); ?>
