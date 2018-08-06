86e17ffcf0c16505dcd0bc7d206458c7ee930dc7
Bloody@DESKTOP-INU2790 <mathieu_franon@outlook.fr>
2018-08-06 10:55:11 +0200

    ajout de service

---
+++ service/FlashService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace projet3\Bloody\service;
+
+class FlashService
+{
+    /**
+     * FlashService constructor.
+     */
+    public function __construct()
+    {
+    	if (session_status() === PHP_SESSION_NONE) {
+    		session_start();
+    	}
+    }
+
+    /**
+     * Crée un message flash en session
+     *
+     * @param $message
+     * @param string $type
+     */
+    public function setFlash($message, $type = 'danger')
+    {
+    	$_SESSION['flashMessage'] = [
+    		'type' => $type, 
+    		'message' => $message
+    	];
+    }
+
+    /**
+     * Affichage des messages flash et suppression de la session
+     */
+    public function showFlashMessage()
+    {
+    	if (isset($_SESSION['flashMessage'])) {
+    		echo '<div class="alert alert-' . $_SESSION['flashMessage']['type'] . '" role="alert">';
+    		echo '<strong>'. $_SESSION['flashMessage']['message'] . '</strong>';
+    		echo '</div>';
+
+    		unset($_SESSION['flashMessage']);
+    	}
+    }
+}
